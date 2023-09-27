1e80a9f24068f4703ca1059a9aca1dd5ab0be080
NachoSanders <steve.h.sanders@gmail.com>
2023-09-26 21:40:35 -0700

    Did project

---
+++ Python Projects/favcolor.py
@@ -0,0 +1,2 @@
+fav_color=input('What is your favorite color? ')
+print('Your favorite color is '+ fav_color +'. Thats a good choice!')
---
+++ Python Projects/names.py
@@ -0,0 +1,3 @@
+first_name=input('Please enter your first name: ')
+last_name=input('Please enter your last name: ')
+print('Hello, my name is ' + last_name + ', ' + first_name + ' ' + last_name + '.')
